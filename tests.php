cc35ff265f7a9de1265c1b305a4ebbf377ed7add
eduardo.arlandi <eduardo.arlandi@debian-8-amd64.fi.uncoma.edu.ar>
2023-06-21 19:29:49 -0300

    Refresh

--- tests.php
+++ tests.php
@@ -1,23 +1,5 @@
 <?php
 
-/* function cargarJuegos(){
-
-    $coleccionJuegos[0] = ["jugadorCruz"=> "Sandra" , "jugadorCirculo" => "Mario", "puntosCruz"=> 5, “puntosCirculo” => 0];
-    $coleccionJuegos[1] = ["jugadorCruz"=> "Mario" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 1, “puntosCirculo” => 1];
-    $coleccionJuegos[2] = ["jugadorCruz"=> "German" , "jugadorCirculo" => "Roberto", "puntosCruz"=> 2, “puntosCirculo” => 0];
-    $coleccionJuegos[3] = ["jugadorCruz"=> "Lucas" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 1, “puntosCirculo” => 1];
-    $coleccionJuegos[4] = ["jugadorCruz"=> "Violeta" , "jugadorCirculo" => "Ana", "puntosCruz"=> 0, “puntosCirculo” => 0];
-    $coleccionJuegos[5] = ["jugadorCruz"=> "Lucia" , "jugadorCirculo" => "Jazmin", "puntosCruz"=> 2, “puntosCirculo” => 0];
-    $coleccionJuegos[6] = ["jugadorCruz"=> "Romina" , "jugadorCirculo" => "German", "puntosCruz"=> 1, “puntosCirculo” => 1];
-    $coleccionJuegos[7] = ["jugadorCruz"=> "Jennifer" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 5, “puntosCirculo” => 0];
-    $coleccionJuegos[8] = ["jugadorCruz"=> "Alberto" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 0, “puntosCirculo” => 2];
-    $coleccionJuegos[9] = ["jugadorCruz"=> "Roberto" , "jugadorCirculo" => "Mario", "puntosCruz"=> 0, “puntosCirculo” => 5];
-
-    return $coleccionJuegos;
-}
-
-*/
-
 /** Determina si un numero esta entre el rango de 1 y 7, en caso de ser valido devuelve el numero ingresado.
  * Si el numero no es valido, sigue pidiendo un numero hasta que el ingresado este dentro de los parametros esperados (1-7) 
  * @param $numOpcion
@@ -25,7 +7,7 @@
 */
 function numeroValido($numOpcionUser){
 
-    while($numOpcionUser<= 1 || $numOpcionUser>7){
+    while($numOpcionUser<1 || $numOpcionUser>7){
         echo "El numero ingresado es incorecto, ingrese nuevamente su opcion numerica: ";
         $numOpcionUser = trim(fgets(STDIN));
 }
@@ -34,7 +16,7 @@ return $numOpcionUser;
 
 /** Dispone un menu del juego tateti con diferentes opciones
 * @param ARRAY $menuOpciones
-* @return ARRAY
+* @return INT
 */
 function seleccionarOpciones (){
 
@@ -58,14 +40,33 @@ function seleccionarOpciones (){
     $numOpcionUser = trim(fgets(STDIN));
     $numOpcionUserValido = numeroValido($numOpcionUser); // Consultamos si el numero esta dentro de los rangos, en caso de no estarlo.. la funcion entra en loop hasta que se elige la opcion correcta.
 
-return $menuOpciones[$numOpcionUserValido];
+return $numOpcionUserValido; // Debe retornar el numero del menu elegido.
 }
 
 
 /* Principal */
 
 
-echo "JUEGO TATETI"."\n";
+echo "Juego del TaTeTi"."\n";
 $opcionElegidaUser = seleccionarOpciones();
 echo $opcionElegidaUser."\n";
 
+
+
+/* function cargarJuegos(){
+
+    $coleccionJuegos[0] = ["jugadorCruz"=> "Sandra" , "jugadorCirculo" => "Mario", "puntosCruz"=> 5, “puntosCirculo” => 0];
+    $coleccionJuegos[1] = ["jugadorCruz"=> "Mario" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 1, “puntosCirculo” => 1];
+    $coleccionJuegos[2] = ["jugadorCruz"=> "German" , "jugadorCirculo" => "Roberto", "puntosCruz"=> 2, “puntosCirculo” => 0];
+    $coleccionJuegos[3] = ["jugadorCruz"=> "Lucas" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 1, “puntosCirculo” => 1];
+    $coleccionJuegos[4] = ["jugadorCruz"=> "Violeta" , "jugadorCirculo" => "Ana", "puntosCruz"=> 0, “puntosCirculo” => 0];
+    $coleccionJuegos[5] = ["jugadorCruz"=> "Lucia" , "jugadorCirculo" => "Jazmin", "puntosCruz"=> 2, “puntosCirculo” => 0];
+    $coleccionJuegos[6] = ["jugadorCruz"=> "Romina" , "jugadorCirculo" => "German", "puntosCruz"=> 1, “puntosCirculo” => 1];
+    $coleccionJuegos[7] = ["jugadorCruz"=> "Jennifer" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 5, “puntosCirculo” => 0];
+    $coleccionJuegos[8] = ["jugadorCruz"=> "Alberto" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 0, “puntosCirculo” => 2];
+    $coleccionJuegos[9] = ["jugadorCruz"=> "Roberto" , "jugadorCirculo" => "Mario", "puntosCruz"=> 0, “puntosCirculo” => 5];
+
+    return $coleccionJuegos;
+}
+
+*/
