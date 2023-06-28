d9464d1bb01bb12eb162d1a25210da5516502c73
arlandiedu <arlandieduardo@gmail.com>
2023-06-28 03:17:40 -0300

    Agregando opcion 5 y funcion TP7

--- programaArlandi.php
+++ programaArlandi.php
@@ -16,24 +16,36 @@ include_once("tateti.php");
 //                  -- PRIMER PUNTO DEL TRABAJO PRACTICO  --
 
  /**                      
- * Funcion destinada a la carga de juegos de array multidimensional para usarlos de ejemplos.
+ * Funcion destinada a la carga de juegos de array multidimensional para usarlos de ejemplos. (20 Ejemplos en total)
  *        Al no tener un parametro formal, la declaracion de parametros esta vacia
  * @return ARRAY
  */
 
 function cargarJuegos(){
 
-    $coleccionJuegos[0] = ["jugadorCruz"=> "Sandra" , "jugadorCirculo" => "Mario", "puntosCruz"=> 5, "puntosCirculo" => 0];
-    $coleccionJuegos[1] = ["jugadorCruz"=> "Mario" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 1, "puntosCirculo" => 1];
-    $coleccionJuegos[2] = ["jugadorCruz"=> "German" , "jugadorCirculo" => "Roberto", "puntosCruz"=> 2, "puntosCirculo" => 0];
-    $coleccionJuegos[3] = ["jugadorCruz"=> "Lucas" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 1, "puntosCirculo" => 1];
-    $coleccionJuegos[4] = ["jugadorCruz"=> "Violeta" , "jugadorCirculo" => "Ana", "puntosCruz"=> 0, "puntosCirculo" => 0];
-    $coleccionJuegos[5] = ["jugadorCruz"=> "Mario" , "jugadorCirculo" => "Jazmin", "puntosCruz"=> 2, "puntosCirculo" => 0];
-    $coleccionJuegos[6] = ["jugadorCruz"=> "Romina" , "jugadorCirculo" => "German", "puntosCruz"=> 1, "puntosCirculo" => 1];
-    $coleccionJuegos[7] = ["jugadorCruz"=> "Jennifer" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 5, "puntosCirculo" => 0];
-    $coleccionJuegos[8] = ["jugadorCruz"=> "Alberto" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 0, "puntosCirculo" => 2];
-    $coleccionJuegos[9] = ["jugadorCruz"=> "Roberto" , "jugadorCirculo" => "Mario", "puntosCruz"=> 0, "puntosCirculo" => 5];
-                    
+    $coleccionJuegos[0] = ["jugadorCruz"=> "Sandra" , "jugadorCirculo" => "Mario", "puntosCruz"=> 5, "puntosCirculo" => 0]; // Sandra 
+    $coleccionJuegos[1] = ["jugadorCruz"=> "Mario" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 1, "puntosCirculo" => 1];  // Empate
+    $coleccionJuegos[2] = ["jugadorCruz"=> "German" , "jugadorCirculo" => "Roberto", "puntosCruz"=> 2, "puntosCirculo" => 0]; // German
+    $coleccionJuegos[3] = ["jugadorCruz"=> "Roberto" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 1, "puntosCirculo" => 1]; // Empate
+    $coleccionJuegos[4] = ["jugadorCruz"=> "Violeta" , "jugadorCirculo" => "Ana", "puntosCruz"=> 4, "puntosCirculo" => 0]; // Violeta
+    $coleccionJuegos[5] = ["jugadorCruz"=> "Mario" , "jugadorCirculo" => "Jazmin", "puntosCruz"=> 2, "puntosCirculo" => 0]; // Mario
+    $coleccionJuegos[6] = ["jugadorCruz"=> "Romina" , "jugadorCirculo" => "German", "puntosCruz"=> 1, "puntosCirculo" => 1]; // Empate
+    $coleccionJuegos[7] = ["jugadorCruz"=> "Jenifer" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 2, "puntosCirculo" => 0]; // Jenifer
+    $coleccionJuegos[8] = ["jugadorCruz"=> "Alberto" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 0, "puntosCirculo" => 2]; // Sandra
+    $coleccionJuegos[9] = ["jugadorCruz"=> "Roberto" , "jugadorCirculo" => "Mario", "puntosCruz"=> 0, "puntosCirculo" => 5]; // Mario
+    $coleccionJuegos[10] = ["jugadorCruz"=> "Sandra" , "jugadorCirculo" => "Jenifer", "puntosCruz"=> 6, "puntosCirculo" => 0]; // Sandra
+    $coleccionJuegos[11] = ["jugadorCruz"=> "Lucas" , "jugadorCirculo" => "Mario", "puntosCruz"=> 0, "puntosCirculo" => 2]; // Mario
+    $coleccionJuegos[12] = ["jugadorCruz"=> "Roberto" , "jugadorCirculo" => "German", "puntosCruz"=> 0, "puntosCirculo" => 3]; // German
+    $coleccionJuegos[13] = ["jugadorCruz"=> "Lucas" , "jugadorCirculo" => "Alberto", "puntosCruz"=> 0, "puntosCirculo" => 6]; // Alberto
+    $coleccionJuegos[14] = ["jugadorCruz"=> "Roberto" , "jugadorCirculo" => "German", "puntosCruz"=> 3, "puntosCirculo" => 0]; // Roberto
+    $coleccionJuegos[15] = ["jugadorCruz"=> "Sandra" , "jugadorCirculo" => "Mario", "puntosCruz"=> 0, "puntosCirculo" => 5]; // Mario
+    $coleccionJuegos[16] = ["jugadorCruz"=> "Ana" , "jugadorCirculo" => "Romina", "puntosCruz"=> 6, "puntosCirculo" => 0]; // Ana
+    $coleccionJuegos[17] = ["jugadorCruz"=> "Mario" , "jugadorCirculo" => "Romina", "puntosCruz"=> 0, "puntosCirculo" => 5]; // Romina
+    $coleccionJuegos[18] = ["jugadorCruz"=> "Lucas" , "jugadorCirculo" => "Sandra", "puntosCruz"=> 0, "puntosCirculo" => 3]; // Sandra
+    $coleccionJuegos[19] = ["jugadorCruz"=> "Jenifer" , "jugadorCirculo" => "Violeta", "puntosCruz"=> 1, "puntosCirculo" => 1]; // Empate
+
+    // Mas ganadora sandra, Lucas nunca gano.
+
     return $coleccionJuegos;
 }     
 
@@ -127,18 +139,78 @@ function agregarJuego($coleccion, $juegoAgregar){
 function primerJuegoGanadoxJugador($coleccionTotal, $jugadorGanador){
 
 
-    foreach ($coleccionTotal as $indice => $juego) { // En este caso, como se trata de un array multidimensional, $indice toma la posicion del index y $juego la informacion del array asociativo.
+    foreach ($coleccionTotal as $indice => $juego) { // Aca usamos $indice no para indicar el recorrido, sino para usarlo de retorno de nuestra funcion.
+
         $jugadorX =  strtolower($juego["jugadorCruz"]); // Uso la funcion strtolower como en el programa principal para que el string devuelto se convierta todo a minuscula.
-        $jugadorO =  strtolower($juego["jugadorCirculo"]); // Uso la funcion strtolower como en el programa principal para que el string devuelto se convierta todo a minuscula.
+        $jugadorO =  strtolower($juego["jugadorCirculo"]); // Igual que arriba.
         $puntosX = $juego["puntosCruz"];
         $puntosO = $juego["puntosCirculo"];
         
-        if (($jugadorX == $jugadorGanador && $puntosX > $puntosO) || ($jugadorO == $jugadorGanador && $puntosX < $puntosO)) { // Aca verificamos si en la iteracion del bucle, el nombre que dimos por parametro coincide con el del nombre iterado y si gano, siendo X u O
-            return $indice; // Si gana alguna vez, retornamos el numero del juego ganado (el indice en el que se encuentra)
+             if (($jugadorX == $jugadorGanador && $puntosX > $puntosO) || ($jugadorO == $jugadorGanador && $puntosX < $puntosO)) { // Aca verificamos si en la iteracion del bucle, el nombre que dimos por parametro coincide con el del nombre iterado y si gano, siendo X u O
+                 return $indice; // Si gana alguna vez, retornamos el numero del juego ganado (el indice en el que se encuentra)
         }
     }
     return -1; 
 }
+
+//               -- SEPTIMO PUNTO DEL TRABAJO PRACTICO -- 
+
+/** Con los parametros obtenidos, la funcion desgloza el array del jugador requerido y brinda informacion del mismo
+ * (Juegos ganados, juegos perdidos, juegos empatados y puntos acumulados en total)
+* @param ARRAY $coleccionTotal
+* @param STRING $nombreJugador
+* @return ARRAY
+*/
+
+
+function resumenJugador($coleccionTotal, $nombreJugador) {
+    
+    $resumen = array( // Declaramos un nuevo array para completar la informacion individual del jugador requerido por parametro.
+        'nombre' => $nombreJugador,
+        'juegosGanados' => 0, // Inicializamos las variables contadora
+        'juegosPerdidos' => 0, // Contadora
+        'juegosEmpatados' => 0, // Contadora
+        'puntosAcumulados' => 0 // Acumulable
+    );
+
+    foreach ($coleccionTotal as $juego) {
+        
+        $jugadorX = strtolower($juego['jugadorCruz']);       
+        $jugadorO = strtolower($juego['jugadorCirculo']);    
+        $puntosX = $juego['puntosCruz'];                     
+        $puntosO = $juego['puntosCirculo'];                 
+
+        if ($jugadorX == strtolower($nombreJugador)) { // Con esta condicion, en cada recorrido comparamos el nombre ingresado por parametro con el iterado, recopilando informacion 
+            if ($puntosX > $puntosO) {
+                $resumen['juegosGanados']++;           // Si gano un juego
+            } elseif ($puntosX < $puntosO) {
+                $resumen['juegosPerdidos']++;          // Si perdio un juego
+            } else {
+                $resumen['juegosEmpatados']++;         // Si lo empato
+            }
+            $resumen['puntosAcumulados'] = $resumen['puntosAcumulados'] + $puntosX;   // En caso de ganar, sumamos los puntos otorgados
+        }
+
+        if ($jugadorO == strtolower($nombreJugador)) { // Mismo caso que arriba pero analizando si el jugador le toco ser O
+            if ($puntosO > $puntosX) {
+                $resumen['juegosGanados'] = $resumen['juegosGanados'] + 1; // Para evitar tanto codigo, resumi con "++".
+            } elseif ($puntosO < $puntosX) {
+                $resumen['juegosPerdidos']++;
+            } else {
+                $resumen['juegosEmpatados']++;
+            }
+            $resumen['puntosAcumulados'] += $puntosO; // Para evitar tanto codigo, resumi con "+=".
+        }
+    }
+
+    return $resumen;
+}
+
+
+
+
+
+
 
 
 
@@ -218,8 +290,18 @@ do {
             break;   
         case 5: 
                 //El usuario eligio la opcion de: 'Mostrar resumen de jugador' 
-    
-            break;
+            echo "Ingresa el nombre del jugador del cual desea un resumen: "."\n";
+            $nombreResumen = strtolower(trim(fgets(STDIN))); 
+            $arrayResumen = resumenJugador($juegos, $nombreResumen); // Creamos un array asociativo para recibir toda la info que nos retorna la funcion.
+
+            echo "*************************************"."\n";
+            echo "Jugador: " .$arrayResumen["nombre"]."\n";
+            echo "Gano: " . $arrayResumen["juegosGanados"] . " juegos"."\n";
+            echo "Perdio: " . $arrayResumen["juegosPerdidos"] . " juegos"."\n";
+            echo "Empato: " . $arrayResumen["juegosEmpatados"] . " juegos"."\n";
+            echo "Total de puntos acumulados: " . $arrayResumen["puntosAcumulados"] . " puntos"."\n";
+            echo "*************************************"."\n";
+                // Tengo el problema que no me muestra si gano, perdio o empato en caso de pedir el resumen de un juego nuevo.
         case 6: 
                 //El usuario eligio la opcion de: 'Mostrar listado de juegos ordenados por jugador O'
         
